9fb0932e97851ed5c00a0f8f1454d212ab48b696
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2020-09-11 01:46:05 +0300

    ap1302: Merge ap1302.h in ap1302.c
    There's no need for a separate header, merge it with the source file.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- ap1302.c
+++ ap1302.c
@@ -29,9 +29,36 @@
 #include <media/media-entity.h>
 #include <media/v4l2-device.h>
 
-#include "ap1302.h"
-
 #define DRIVER_NAME "AP1302"
+
+#define AP1302_CHIP_ID			0x265
+#define AP1302_REG16			2
+#define AP1302_REG32			4
+#define AP1302_FW_WINDOW_SIZE		0x2000
+#define AP1302_FW_WINDOW_OFFSET		0x8000
+
+#define REG_CHIP_VERSION		0x0000
+#define REG_CHIP_REV			0x0050
+#define REG_SIP_CRC			0xF052
+#define REG_BOOTDATA_STAGE		0x6002
+
+struct ap1302_device {
+	struct device *dev;
+	struct v4l2_subdev sd;
+	const struct firmware *fw;
+	struct media_pad pad;
+	struct i2c_client *client;
+	u32 cam_config;
+	struct v4l2_mbus_framefmt formats[1];
+	struct regmap *regmap16;
+	struct regmap *regmap32;
+	struct gpio_desc *reset_gpio;
+};
+
+struct ap1302_firmware_header {
+	u16 pll_init_size;
+	u16 crc;
+} __packed;
 
 /* There's no standard V4L2_CID_GREEN_BALANCE defined in the
  * linux kernel. Let's borrow V4L2_CID_CHROMA_GAIN on green
